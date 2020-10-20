13ee9f0b0687b3a49aea78de3c26d4b6532f80d4
Michael Borders <sysrabbit@ivalice.cc>
2020-10-20 14:00:12 -0400

    Replace all instances of ghacks with arkenfox

--- patch.sh
+++ patch.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 # Check if a copy of the ghack git repo survived
-if [ -d /tmp/ghacks ]; then
-    if [ -d /tmp/ghacks/.git ]; then
+if [ -d /tmp/arkenfox ]; then
+    if [ -d /tmp/arkenfox/.git ]; then
 	# Git repo still exists, forcibly update it
-	(cd /tmp/ghacks &&
+	(cd /tmp/arkenfox &&
 	    git reset --hard &&
 	    git fetch --all &&
 	    git rebase origin/master)
     else
 	# Something exists, but it's not the git repo
 	# Delete it with confirmation
-	rm -r /tmp/ghacks
+	rm -r /tmp/arkenfox
 	# Get the latest version
-	git clone https://github.com/arkenfox/user.js.git /tmp/ghacks
+	git clone https://github.com/arkenfox/user.js.git /tmp/arkenfox
     fi
 else
     # Get the latest version
-    git clone https://github.com/arkenfox/user.js.git /tmp/ghacks
+    git clone https://github.com/arkenfox/user.js.git /tmp/arkenfox
 fi
 
 # Patch it
 for patch in patches/*.patch; do
 	echo "=== Applying $patch ==="
-    patch -p0 -i $patch /tmp/ghacks/user.js
+    patch -p0 -i $patch /tmp/arkenfox/user.js
 done
 
 # Copy it to the repo
-cp /tmp/ghacks/user.js .
+cp /tmp/arkenfox/user.js .
